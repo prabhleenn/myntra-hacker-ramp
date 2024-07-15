2e65aec8c744eec06d101dc79f52ab5521b948df
Prabhleen Kaur <114502930+prabhleenn@users.noreply.github.com>
2024-07-15 11:59:30 +0530

    Update train.py

--- train.py
+++ train.py
@@ -4,5 +4,5 @@ from ultralytics import YOLO
 model = YOLO('/content/drive/MyDrive/yolov8/yolov8n.pt')  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
-results = model.train(data='/content/drive/MyDrive/yolov8-Vehicle-Detection/config/data.yaml', epochs=100, imgsz=640, save_period=10)
+results = model.train(data='/content/drive/MyDrive/myntra-yolov8/config/data.yaml', epochs=100, imgsz=640, save_period=10)
 model.val()
